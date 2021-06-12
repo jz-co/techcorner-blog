040517d8974af9bdd395ffa20713f8fc78daa3f9
Jenny <jennny.zhang@mail.utoronto.ca>
2021-06-12 10:22:40 -0400

    removed linkOverlay for hero post card

--- components/hero-post-card.js
+++ components/hero-post-card.js
@@ -7,25 +7,22 @@ import { Flex, Button, Box, Heading, Text, LinkBox, LinkOverlay, Image } from '@
  */
 export default function HeroPostCard({ title, caption, publishedDate, tag, image, link, ...props }) {
 	return (
-		<LinkBox>
-			<Flex maxW="780px" maxH={["1000px", "240px"]} {...props} overflow='hidden' flexWrap="wrap">
-				<Box d="flex" flexBasis="50%">
-					<Image src={image} objectFit="cover"/>
-				</Box>
-				<Box p={8} bg='white' color='#353535' flexBasis="50%">
-					<Button as="a" href="/notes" variant='outline' size='sm' fontSize='xs' p={2} textTransform='uppercase'>
-						{tag}
-                        {/* Replace with PostTag Component  */}
-					</Button>
-					<Heading fontSize='x-large' mb={2} mt={2} maxW="100%">
-						<Link href={link}>
-							<LinkOverlay>{title}</LinkOverlay>
-						</Link>
-					</Heading>
-					<Text  fontSize='xs'>{caption}</Text>
-					<Text color='#959595' fontSize='xx-small' mt={4}>{`Published ${publishedDate}`}</Text>
-				</Box>
-			</Flex>
-		</LinkBox>
+		<Flex maxW='780px' maxH={['1000px', '240px']} {...props} overflow='hidden' flexWrap='wrap'>
+			{/* Still debating whether to use Next link instead since it gives power to smoother page transitions */}
+			<Box as="a" href={link} d='flex' flexBasis='50%'>
+				<Image src={image} objectFit='cover' />
+			</Box>
+			<Box p={8} bg='white' color='#353535' flexBasis='50%'>
+				<Button as='a' href='/notes' variant='outline' size='sm' fontSize='xs' p={2} textTransform='uppercase'>
+					{tag}
+					{/* Replace with PostTag Component  */}
+				</Button>
+				<Heading fontSize='x-large' mb={2} mt={2} maxW='100%'>
+					<Link href={link}>{title}</Link>
+				</Heading>
+				<Text fontSize='xs'>{caption}</Text>
+				<Text color='#959595' fontSize='xx-small' mt={4}>{`Published ${publishedDate}`}</Text>
+			</Box>
+		</Flex>
 	);
 }
