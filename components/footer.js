d192d0ebddad84c5df14e89d3733c69a13742538
Jessie Lam <jessielam@MacBook-Pro.local>
2021-08-01 18:26:02 -0400

    Made changes to footer to incorporate colour theme. Updated about page

--- components/footer.js
+++ components/footer.js
@@ -7,22 +7,26 @@ export default function Footer() {
             <Flex justifyContent="center" marginTop="30px">
                 <Box as="footer" fontWeight="550" width="83%">
                     <Flex justifyContent="center">
-                        <Flex flexWrap="wrap"  alignItems="flex-start">
-                            <Box color="#FAFAFA" width="240px" padding="20px">
-                                <svg width='64' height='53' viewBox='0 0 296 260' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                    <path d='M251.601 103.354L95.7601 40.6034L33.0091 196.444' stroke='#63C1F6' stroke-width='13' />
-                                    <circle cx='32.8828' cy='202.883' r='25' transform='rotate(-23.4451 32.8828 202.883)' fill='#5FC5F0' />
-                                    <circle cx='92.8828' cy='33.8615' r='25' transform='rotate(-23.4451 92.8828 33.8615)' fill='#6DB6EB' />
-                                    <circle cx='262.883' cy='115.904' r='25' transform='rotate(-23.4451 262.883 115.904)' fill='#63D3F7' />
-                                    <circle cx='50.3062' cy='150.306' r='20' transform='rotate(-23.4451 50.3062 150.306)' fill='#63D3F7' />
-                                    <circle cx='72.018' cy='95.018' r='17.5' transform='rotate(-23.4451 72.018 95.018)' fill='#63D3F7' />
-                                    <circle cx='181.018' cy='74.018' r='17.5' transform='rotate(-23.4451 181.018 74.018)' fill='#63D3F7' />
-                                    <line x1='46.53' y1='151.653' x2='178.53' y2='76.6527' stroke='#63D3F6' stroke-width='10' />
-                                    <line x1='60.4816' y1='95.0232' x2='246.482' y2='113.023' stroke='#63D3F6' stroke-width='10' />
-                                </svg>
-                                <Heading size="md" paddingTop="0.5rem">TECH CORNER</Heading>
-                                <Text fontSize="xs" mt="5px"> &copy; Tech Corner 2021</Text>
-                            </Box>
+                        <Flex flexWrap="wrap" alignItems="flex-start">
+                            <Link href="/" _hover={{ textDecoration: "none" }}>
+
+                                <Box color="#FAFAFA" width="240px" padding="20px">
+                                    <svg width='64' height='53' viewBox='0 0 296 260' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                                        <path d='M251.601 103.354L95.7601 40.6034L33.0091 196.444' stroke='#63C1F6' stroke-width='13' />
+                                        <circle cx='32.8828' cy='202.883' r='25' transform='rotate(-23.4451 32.8828 202.883)' fill='#5FC5F0' />
+                                        <circle cx='92.8828' cy='33.8615' r='25' transform='rotate(-23.4451 92.8828 33.8615)' fill='#6DB6EB' />
+                                        <circle cx='262.883' cy='115.904' r='25' transform='rotate(-23.4451 262.883 115.904)' fill='#63D3F7' />
+                                        <circle cx='50.3062' cy='150.306' r='20' transform='rotate(-23.4451 50.3062 150.306)' fill='#63D3F7' />
+                                        <circle cx='72.018' cy='95.018' r='17.5' transform='rotate(-23.4451 72.018 95.018)' fill='#63D3F7' />
+                                        <circle cx='181.018' cy='74.018' r='17.5' transform='rotate(-23.4451 181.018 74.018)' fill='#63D3F7' />
+                                        <line x1='46.53' y1='151.653' x2='178.53' y2='76.6527' stroke='#63D3F6' stroke-width='10' />
+                                        <line x1='60.4816' y1='95.0232' x2='246.482' y2='113.023' stroke='#63D3F6' stroke-width='10' />
+                                    </svg>
+                                    <Heading size="md" paddingTop="0.5rem">TECH CORNER</Heading>
+                                    <Text fontSize="xs" mt="5px"> &copy; Tech Corner {new Date().getFullYear()}</Text>
+                                
+                                </Box>
+                            </Link>
                             <Box color="#FAFAFA" width="290px" padding="20px">
                                 <Text color="#90E4FF" fontSize="1.1rem" pt="0.25rem" fontWeight="600" mt="0.8rem" mb="0.6rem"> Contact Us </Text>
                                 <HStack paddingBottom="0.3rem">
@@ -31,26 +35,26 @@ export default function Footer() {
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3422 5.62779C10.4822 5.62757 10.6384 5.62779 10.8139 5.62779C12.2177 5.62779 12.3841 5.6329 12.9385 5.65845C13.4511 5.68223 13.7293 5.76912 13.9147 5.84212C14.16 5.93879 14.335 6.05435 14.5189 6.24102C14.7029 6.42768 14.8168 6.60546 14.9123 6.85436C14.9843 7.04214 15.0701 7.32436 15.0934 7.84437C15.1186 8.40659 15.1241 8.57549 15.1241 9.99883C15.1241 11.4222 15.1186 11.5911 15.0934 12.1533C15.07 12.6733 14.9843 12.9555 14.9123 13.1433C14.817 13.3922 14.7029 13.5694 14.5189 13.756C14.3348 13.9427 14.1601 14.0582 13.9147 14.1549C13.7295 14.2282 13.4511 14.3149 12.9385 14.3387C12.3842 14.3642 12.2177 14.3698 10.8139 14.3698C9.40992 14.3698 9.24353 14.3642 8.68927 14.3387C8.17663 14.3147 7.89841 14.2278 7.71296 14.1548C7.4676 14.0581 7.29234 13.9425 7.10831 13.7559C6.92429 13.5692 6.81037 13.3919 6.71485 13.1429C6.64289 12.9551 6.55712 12.6729 6.53379 12.1529C6.50859 11.5906 6.50355 11.4217 6.50355 9.9975C6.50355 8.57326 6.50859 8.40526 6.53379 7.84303C6.55723 7.32303 6.64289 7.0408 6.71485 6.8528C6.81015 6.60391 6.92429 6.42613 7.10831 6.23946C7.29234 6.05279 7.4676 5.93724 7.71296 5.84035C7.8983 5.76701 8.17663 5.68034 8.68927 5.65645C9.1743 5.63423 9.36227 5.62757 10.3422 5.62645V5.62779ZM13.6204 6.51335C13.2721 6.51335 12.9895 6.79969 12.9895 7.15314C12.9895 7.50647 13.2721 7.79314 13.6204 7.79314C13.9688 7.79314 14.2514 7.50647 14.2514 7.15314C14.2514 6.7998 13.9688 6.51335 13.6204 6.51335ZM10.8139 7.26114C9.32273 7.26114 8.11376 8.48748 8.11376 10.0001C8.11376 11.5126 9.32273 12.7384 10.8139 12.7384C12.305 12.7384 13.5135 11.5126 13.5135 10.0001C13.5135 8.48748 12.305 7.26114 10.8139 7.26114Z" fill="#FAFAFA"/>
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M10.8139 8.22226C11.7817 8.22226 12.5665 9.01816 12.5665 10.0001C12.5665 10.9818 11.7817 11.7779 10.8139 11.7779C9.84588 11.7779 9.06126 10.9818 9.06126 10.0001C9.06126 9.01816 9.84588 8.22226 10.8139 8.22226Z" fill="#FAFAFA"/>
                                     </svg>
-                                    <Link href="#"><Text> @techcorner </Text></Link>
+                                    <Link href="https://www.instagram.com/techcorner0101/"><Text> @techcorner0101 </Text></Link>
                                 </HStack>
                                 <HStack>
                                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M0 10C0 4.47715 4.41371 0 9.85829 0C15.3029 0 19.7166 4.47715 19.7166 10C19.7166 15.5228 15.3029 20 9.85829 20C4.41371 20 0 15.5228 0 10ZM11.5018 9.79757H11.5013V13H15.159C15.4841 13 15.7733 12.8023 15.7733 12.3308V8C15.7733 8 12.3236 10.3916 12.4455 10.3138C12.4455 10.3138 12.3232 10.3916 12.2218 10.3138L11.5018 9.79757ZM11.5013 6.66667V9.12139L12.2904 9.66154C12.3112 9.66814 12.3563 9.66861 12.3771 9.66154L15.7733 7.17241C15.7733 6.87782 15.5205 6.66667 15.3779 6.66667H11.5013ZM4.27193 5.35211V14.5352L11.1727 16V4L4.27193 5.35211Z" fill="#FAFAFA"/>
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M8.88968 11.4961C8.59806 11.8846 8.21785 12.0793 7.7486 12.0793C7.29136 12.0793 6.91916 11.8909 6.63111 11.5146C6.3435 11.1378 6.19925 10.6474 6.19925 10.0426C6.19925 9.40394 6.34528 8.88744 6.63779 8.49307C6.93029 8.0987 7.31763 7.9013 7.7998 7.9013C8.25525 7.9013 8.62389 8.08969 8.90481 8.46738C9.18619 8.84507 9.32688 9.34265 9.32688 9.96056C9.32732 10.5956 9.18129 11.1076 8.88968 11.4961ZM7.73933 8.66667C7.48278 8.66667 7.27837 8.78926 7.12743 9.03399C6.9765 9.27872 6.9008 9.60272 6.9008 10.006C6.9008 10.4153 6.9765 10.7388 7.12743 10.9766C7.27837 11.2149 7.47644 11.3333 7.7212 11.3333C7.97366 11.3333 8.174 11.2177 8.32176 10.9863C8.46952 10.7549 8.54385 10.4337 8.54385 10.023C8.54385 9.59488 8.47224 9.26167 8.32856 9.02339C8.18488 8.78557 7.98862 8.66667 7.73933 8.66667Z" fill="#FAFAFA"/>
                                     </svg>
-                                    <Link href="#"><Text>techcorner@tech.com</Text></Link>
+                                    <Link href="mailto:techcorner0101@gmail.com"><Text>techcorner0101@gmail.com</Text></Link>
                                 </HStack>
                             </Box>
                             <Box color="#FAFAFA" width="150px" padding="20px">
                                 <Text color="#90E4FF" fontSize="1.1rem" pt="0.25rem" fontWeight="600" mt="0.8rem" mb="0.6rem"> Pages </Text>
                                 <Box pb="0.5rem">
-                                    <Link href="#">Learn</Link>
+                                    <Link href="/notes">Learn</Link>
                                 </Box>
                                 <Box pb="0.5rem">
-                                    <Link href="#">Blog</Link>
+                                    <Link href="/blog">Blog</Link>
                                 </Box>
                                 <Box  pb="0.5rem">
-                                    <Link href="#">About Us</Link>
+                                    <Link href="/about">About Us</Link>
                                 </Box>
                             </Box>
 
